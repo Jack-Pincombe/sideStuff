91d9fc10b0fc910cb55a342cc09dfbaa1332f5e4
Jack Pincombe <jackpincombe1@gmail.com>
2016-12-19 14:42:57 +0000

    Some of the beginning functions made

--- rps1.py
+++ rps1.py
@@ -19,9 +19,35 @@ computer_score = 0
 
 def start():
 
+    print "lets play a game of rock, paper , scissors"
+
+    while game():
+        pass
+    scores()
+
+
+
 def game():
+    player = move()
+    computer = random.randint(1, 3)
+
+    result(player, computer)
+    return play_again()
 
 def move():
+
+    while True:
+        print
+        player_input = raw_input("Rock 1\n, paper 2\n and scissors /3\n make a move")
+        try:
+            player = int(player_input)
+            if player in (1,2,3):
+                return player
+        except ValueError:
+            pass
+        print "Please enter 1, 2 or 3"
+
+
 
 def result():
 
